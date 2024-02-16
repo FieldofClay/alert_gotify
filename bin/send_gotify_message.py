93d315fdc3963c3d8f60f8bbf4ddc47d346bcc99
FieldofClay <7278759+FieldofClay@users.noreply.github.com>
2024-02-16 14:26:23 +1100

    improved logging

--- bin/send_gotify_message.py
+++ bin/send_gotify_message.py
@@ -1,28 +1,45 @@
-import sys, requests, json, re
+import sys, requests, json, re, os
+import logging, logging.handlers
+import splunk
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+def setup_logging():
+    logger = logging.getLogger('splunk.gotify')    
+    SPLUNK_HOME = os.environ['SPLUNK_HOME']
+    
+    LOGGING_DEFAULT_CONFIG_FILE = os.path.join(SPLUNK_HOME, 'etc', 'log.cfg')
+    LOGGING_LOCAL_CONFIG_FILE = os.path.join(SPLUNK_HOME, 'etc', 'log-local.cfg')
+    LOGGING_STANZA_NAME = 'python'
+    LOGGING_FILE_NAME = "gotify.log"
+    BASE_LOG_PATH = os.path.join('var', 'log', 'splunk')
+    LOGGING_FORMAT = "%(asctime)s %(levelname)-s\t%(module)s:%(lineno)d - %(message)s"
+    splunk_log_handler = logging.handlers.RotatingFileHandler(os.path.join(SPLUNK_HOME, BASE_LOG_PATH, LOGGING_FILE_NAME), mode='a') 
+    splunk_log_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+    logger.addHandler(splunk_log_handler)
+    splunk.setupSplunkLogger(logger, LOGGING_DEFAULT_CONFIG_FILE, LOGGING_LOCAL_CONFIG_FILE, LOGGING_STANZA_NAME)
+    return logger
 
-def check_inputs(config):
+
+def check_inputs(config, logger):
     setup_fields = ['url', 'token']
     required_fields = ['message', 'priority']
     
     for field in setup_fields:
         if not field in config:
-            eprint("ERROR No "+field+" specified. Have you configured the addon?")
+            logger.error("No "+field+" specified. Have you configured the addon?")
             return False
     
     for field in required_fields:
         if not field in config:
-            eprint("ERROR, No "+field+" specified.")
+            logger.error("No "+field+" specified.")
             return False
         
     return True
 
 
 if len(sys.argv) > 1 and sys.argv[1] == "--execute":
+    logger = setup_logging()
     alert = json.load(sys.stdin)
-    if check_inputs(alert['configuration']):
+    if check_inputs(alert['configuration'], logger):
         #load config
         config = alert['configuration']
         ssl_verify = True
@@ -49,11 +66,11 @@ if len(sys.argv) > 1 and sys.argv[1] == "--execute":
         #doit
         r = requests.post(url,headers=headers,json=payload,verify=ssl_verify)
         if r.status_code == 200:
-            eprint("INFO 200: Success sending message")
+            logger.info("200: Success sending message")
         else:
-            eprint("ERROR "+str(r.status_code)+": "+r.text)
+            logger.error(str(r.status_code)+": "+r.text)
 
     else:
-        eprint("ERROR Invalid configuration detected. Stopped.")
+        logger.error("Invalid configuration detected. Stopped.")
 else:
-    eprint("FATAL No execute flag given")
+    print("FATAL No execute flag given")
